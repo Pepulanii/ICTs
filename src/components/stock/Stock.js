5eb27342b6a78bc80ab470cc8ba5da3b8fc6d045
pterolycus9pn <pngwira7@gmail.com>
2021-04-19 09:48:06 +0200

    more commit

---
+++ src/components/stock/Stock.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import const from "../assets/" 
+function Stock() {
+    return (
+        <div className="stock_container">
+            
+        </div>
+    )
+}
+
+export default Stock
